f871638892ccd06c9847aa72c274117acca622b1
jcampbell <jonathan.r.campbell1@gmail.com>
2022-02-25 19:13:17 -0500

    updated dockerfile path

--- HubService/HubService/Controllers/WeatherForecastController.cs
+++ HubService/HubService/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace HubService.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "asdf"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
