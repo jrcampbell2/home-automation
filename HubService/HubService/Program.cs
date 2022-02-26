abd7227343ac2c556a60bfab64f8ca79424e9a14
jcampbell <jonathan.r.campbell1@gmail.com>
2022-02-26 12:17:23 -0500

    removed https redirection

--- HubService/HubService/Program.cs
+++ HubService/HubService/Program.cs
@@ -6,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHttpsRedirection(options => options.HttpsPort = 443);
 
 var app = builder.Build();
 
@@ -16,8 +15,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
