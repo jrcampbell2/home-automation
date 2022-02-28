dea298e30db0d16f16739798dd1d6c46ad40ee88
jcampbell <jonathan.r.campbell1@gmail.com>
2022-02-28 08:08:47 -0500

    Updated to use in-memory db

--- HubService/HubService/Nodes/Infrastructure/NodesContext.cs
+++ HubService/HubService/Nodes/Infrastructure/NodesContext.cs
@@ -6,19 +6,9 @@ namespace HubService.Nodes.Infrastructure
     public class NodesContext : DbContext
     {
         public DbSet<Node> Nodes { get; set; }
-
-        public string DbPath { get; }
-
-        public NodesContext()
-        {
-            var folder = Environment.SpecialFolder.LocalApplicationData;
-            var path = Environment.GetFolderPath(folder);
-            DbPath = System.IO.Path.Join(path, "nodes.db"); // To-do, hard coded path should be moved to configuration.
-        }
-
-        // The following configures EF to create a Sqlite database file in the
-        // special "local" folder for your platform.
+     
+        // In memory database used only for initial application setup testing purposes.
         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseSqlite($"Data Source={DbPath}");
+            => options.UseInMemoryDatabase("InMemoryDatabase");
     }
 }
